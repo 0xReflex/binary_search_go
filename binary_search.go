4aee5e04392b6e23252472a66e66a147ebfb5e9d
unknown <prrprrbro23@gmail.com>
2020-11-08 22:39:41 +0530

    Second commit to the repo made one new function inputs_from_the_user()

--- binary_search.go
+++ binary_search.go
@@ -1,26 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	a := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
-	target := 18
-	fmt.Println(binary_search(a,target))
+	inputs_from_the_user()
 }
-func binary_search(array []int, target int) int{
+
+func inputs_from_the_user() {
+	var target, length_of_array, i int64
+	var list_numbers []int64
+	fmt.Println("what is the length of the array:")
+	fmt.Println("Don't put large numbers or it will kill your pc and then you have to switch it off")
+	fmt.Scanln(&length_of_array)
+	fmt.Println("what is your target:")
+	fmt.Scanln(&target)
+	for i = 0; i <= length_of_array; i++ {
+		list_numbers = append(list_numbers, i)
+		}
+	fmt.Println(binary_search(list_numbers,target))
+}
+
+
+
+// see how_it_works to know how this program works :) 
+func binary_search(array []int64, target int64) (int, string, int){
+	hops := 0
 	left := 0
 	right := len(array) - 1
 	for left <= right {
-		var mid int = (left + right)/2
+		mid := (left + right)/2
 		if array[mid] == target {
-			return mid
+			return mid , "Number of hops:", hops
 		} else if array[mid] < target {
 			left = mid + 1
-			// fmt.Println("we are at left:", left)
 		} else {
 			right = mid - 1
-			// fmt.Println("we are at right:", right)
 		}
+		hops += 1
 	}
-	return -1
+	return -1,"Number of hops:", hops
 }
